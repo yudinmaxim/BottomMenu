77bf0a3786ac73970d0510e6cdf85256c9d523b5
yudinmaxim <61460166+yudinmaxim@users.noreply.github.com>
2023-03-04 18:59:29 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <BottomMenu />
+</template>
+
+<script setup>
+import BottomMenu from "./components/BottomMenu.vue";
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
